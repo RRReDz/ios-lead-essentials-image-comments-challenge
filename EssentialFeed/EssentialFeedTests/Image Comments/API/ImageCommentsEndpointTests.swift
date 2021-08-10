898b9f1f73975cb928610cee115c01513330725c
Riccardo Rossi <riki.redz@gmail.com>
2021-08-10 17:08:38 +0200

    Hardcodes UUID string into test URL variable to avoid any extra logic in tests.

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -9,9 +9,10 @@ class ImageCommentsEndpointTests: XCTestCase {
 	func test_comments_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
 
-		let feedId = UUID()
-		let received = ImageCommentsEndpoint.get(feedId).url(baseURL: baseURL)
-		let expected = URL(string: "http://base-url.com/v1/image/\(feedId)/comments")!
+		let commentsIdString = "11E123D5-1272-4F17-9B91-F3D0FFEC895A"
+		let commentsId = UUID(uuidString: commentsIdString)!
+		let received = ImageCommentsEndpoint.get(commentsId).url(baseURL: baseURL)
+		let expected = URL(string: "http://base-url.com/v1/image/\(commentsIdString)/comments")!
 
 		XCTAssertEqual(received, expected)
 	}
