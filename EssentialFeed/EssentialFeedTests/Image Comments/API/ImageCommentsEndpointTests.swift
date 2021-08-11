4ec0974a2fd736f064cf57e3889198a05012f0e3
Riccardo Rossi <riki.redz@gmail.com>
2021-08-11 17:14:31 +0200

    Removes string interpolation in favour of hardcoded value to reduce logic in tests.

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -9,10 +9,9 @@ class ImageCommentsEndpointTests: XCTestCase {
 	func test_comments_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
 
-		let commentsIdString = "11E123D5-1272-4F17-9B91-F3D0FFEC895A"
-		let commentsId = UUID(uuidString: commentsIdString)!
+		let commentsId = UUID(uuidString: "11E123D5-1272-4F17-9B91-F3D0FFEC895A")!
 		let received = ImageCommentsEndpoint.get(commentsId).url(baseURL: baseURL)
-		let expected = URL(string: "http://base-url.com/v1/image/\(commentsIdString)/comments")!
+		let expected = URL(string: "http://base-url.com/v1/image/11E123D5-1272-4F17-9B91-F3D0FFEC895A/comments")!
 
 		XCTAssertEqual(received, expected)
 	}
